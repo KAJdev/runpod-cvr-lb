d013832116e7b918d78f4454e1d69cf184cbfa98
zeke <40004347+KAJdev@users.noreply.github.com>
2024-12-05 20:52:25 -0900

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,9 +1,13 @@
+import time
 import runpod
 import subprocess
 
 
 def handler(job):
     """get CUDA version"""
+
+    # sleep for 10 seconds to simulate a long running job
+    time.sleep(10)
 
     output = subprocess.check_output(["nvcc", "--version"]).decode("utf-8")
     return output.split("\n")[3]
