9a1f52b46be14d27144950955fa467fabd8f99d2
zeke <40004347+KAJdev@users.noreply.github.com>
2024-12-10 00:02:53 -0900

    return random bytes

--- src/handler.py
+++ src/handler.py
@@ -1,18 +1,13 @@
 import time
 import runpod
-import subprocess
 
 
 def handler(job):
-    """get CUDA version"""
-
     # sleep for 10 seconds to simulate a long running job
     time.sleep(10)
 
-    output = subprocess.check_output(["nvcc", "--version"]).decode("utf-8")
-    return output.split("\n")[3]
+    # return some bytes to test what happens when the output is not a string
+    return bytes(b"asedfkmeaklwfm")
 
-
-# asedfkmeaklwfm
 
 runpod.serverless.start({"handler": handler})
